89ee7dfe3e22923904d9539a12e192cce56c7ed4
David Huerta <huertanix@gmail.com>
2016-06-23 10:20:07 -0400

    Vars and stuff.

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask
+from flask import request
 from twilio import twiml
 import requests
 # test
 
 app = Flask(__name__)
 
-path = "http://finance.yahoo.com/webservice/v1/symbols/AAPL/quote?format=json"
+path = "http://finance.yahoo.com/webservice/v1/symbols/{0}/quote?format=json"
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms():
-    result = requests.get(path)
     response = twiml.Response()
+    body = request.form['Body']
+    result = requests.get(path.format(body))
     price = result.json()['list']['resources'][0]['resource']['fields']['price']
     response.message("The current price of $AAPL is: {0}".format(price))
 
