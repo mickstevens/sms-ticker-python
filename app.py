a93d19212f4620e75d56ee9aacee8957c1d0b039
Rob Spectre <rob@brooklynhacker.com>
2016-06-23 10:28:41 -0400

    Making it dynamic.

--- app.py
+++ app.py
@@ -8,13 +8,17 @@ app = Flask(__name__)
 
 path = "http://finance.yahoo.com/webservice/v1/symbols/{0}/quote?format=json"
 
+
 @app.route("/sms", methods=['GET', 'POST'])
 def sms():
     response = twiml.Response()
     body = request.form['Body']
-    result = requests.get(path.format(body))
-    price = result.json()['list']['resources'][0]['resource']['fields']['price']
-    response.message("The current price of $AAPL is: {0}".format(price))
+    try:
+        result = requests.get(path.format(body))
+        price = result.json()['list']['resources'][0]['resource']['fields']['price']
+        response.message("The current price of {1} is: {0}".format(price, body))
+    except:
+        response.message("We could not find that price.")
 
     return str(response)
 
